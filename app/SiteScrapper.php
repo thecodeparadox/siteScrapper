4f5800a46c447af0617121048cbb3b245ffc2e90
thecodeparadox <dabdullahy@gmail.com>
2015-11-04 23:12:54 +0600

    Adding necessary files

---
+++ app/SiteScrapper.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: abdullah
+ * Date: 11/4/15
+ * Time: 11:10 PM
+ */
+class SiteScrapper
+{
+
+}
---
+++ config/app.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abdullah
+ * Date: 11/4/15
+ * Time: 11:11 PM
+ */
